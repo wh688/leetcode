75e0be8f48c8dc646e82a0ed122732254514deda
HWT <wh688@nyu.edu>
2014-12-25 02:21:27 -0500

    Excel Sheet Column Title

---
+++ src/excelSheetColumnTitle/Solution.java
@@ -0,0 +1,7 @@
+package excelSheetColumnTitle;
+
+public class Solution {
+    public String convertToTitle(int n) {
+    	return n == 0 ? "" : convertToTitle(--n / 26) + (char)('A' + (n % 26));
+    }
+}
